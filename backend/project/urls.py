eef31c425bc963290aa54b76d74715f00f95e1c2
Harrison Chui <chuih@programofstudies.uscsd.k12.pa.us>
2019-04-30 23:11:23 +0000

    updated database

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -24,4 +24,4 @@ admin.site.index_title= ""
 admin.site.site_url= "/app"
 # media file fix in development; not needed in production
 if settings.DEBUG is True:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
